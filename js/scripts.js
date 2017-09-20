72f1284eaa2338c0a75ba306d78152d8d05147f8
9chris5 <kheciek@gmail.com>
2017-09-20 18:30:02 +0200

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var withButtonClass = document.getElementsByClassName('buttonClass');
 
-for ( var i = 0 ; i < withButtonClass.length ; i++) {
+for (var i = 0; i < withButtonClass.length; i++) {
     alert(withButtonClass[i].innerText);
 }
